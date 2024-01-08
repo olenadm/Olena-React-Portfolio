ce69dbdc02e8ca83ade1f08d215a998189d1dd02
olenadm <olenad@gmail.com>
2024-01-08 20:53:36 +0100

    Single quotes escaped

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,8 +6,8 @@ export default function Contact() {
     <section className="blocks  text-center" id="contact">
       <Row>
         <Col md={{ span: 10, offset: 1 }}>
-          <h2 className="mb-md-5 mb-3">0.3. What's Next?</h2>
-          <p>Don't be shy! Feel free to get in touch.</p>
+          <h2 className="mb-md-5 mb-3">0.3. What&rsquo;s Next?</h2>
+          <p>Don&rsquo;t be shy! Feel free to get in touch.</p>
           <Button
             className="btn btn-primary btn-lg mt-4 mb-5"
             href="mailto:olenad@gmail.com"
