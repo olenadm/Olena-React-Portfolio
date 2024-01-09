2d50436ce724a9d266b6b2e3e1c9492c13e35fb8
olenadm <olenad@gmail.com>
2024-01-09 15:59:03 +0100

    Kendo dashboard

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -23,12 +23,12 @@ export const PROJECT_DATA =  [
     },
     {
       id: "2",
-      url: "https://lucent-lolly-15f9c4.netlify.app/",
-      git: `${GIT_URL}/Nitro`,
-      title: "Filtered Tree List",
-      img_url: "/img/react-ui-2.png",
-      tech: ["React.JS", "Next.JS", "Bootstrap"],
+      url: "https://cosmic-malasada-18561b.netlify.app/",
+      git: `${GIT_URL}/KendoDashboard`,
+      title: "Kendo Dashboard",
+      img_url: "/img/kendo.jpg",
+      tech: ["React.JS", "KendoUI", "SCSS"],
       description:
-        "Display posts on a tree view (expandable accordion), where posts are grouped by posting week. Clicking on each element, the user can see all details and a form where 'location' and 'author' fields can be edited. User can change the grouping type of the list. So instead of having a tree grouped by weeks, it could group it by 'author' or 'location'.",
+        "A simple demo Dshboard using some KendoUI components (Grid (with custom Cells, Column Ordering, Filtering), Drawer component for navigating.).",
     },
   ];
