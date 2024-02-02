dac7d103634605f9bb6c56d76c3fc33c6afea141
olenadm <olenad@gmail.com>
2024-02-02 12:16:13 +0100

    GA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <GoogleAnalytics gaId="G-0WBTPTKMNK" />
 
       <body>
         <Main />
@@ -11,6 +14,7 @@ export default function Document() {
         <NextScript />
         
       </body>
+      
     </Html>
   );
 }
